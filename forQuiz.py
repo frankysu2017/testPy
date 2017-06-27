cfb88c6a7488aa6b9ddecf67b2f1b4d65a647da8
frankysu2017 <frankysu@protonmail.com>
2017-06-27 10:36:42 +0800

    Merge branch 'master' of https://github.com/frankysu2017/testPy
    # Conflicts:
    #	judgeThePassword.py

--- forQuiz.py
+++ forQuiz.py
@@ -3,30 +3,18 @@
 
 import pandas as pd
 import numpy as np
+import datetime as dt
+import matplotlib.pyplot as plt
+import pylab as pl
 import time
 
+def MaxClose():
+    listMonth = []
+    quotesMSFT = pd.read_csv(r'./quotes_MSFT.csv', header=0, encoding='gbk', index_col=None)
+    quotesINTC = pd.read_csv(r'./quotes_INTC.csv', header=0, encoding='gbk', index_col=None)
+    listMonth = pd.Series(map(lambda x: time.strptime(x, '%Y-%m-%d')[1], quotesMSFT.date))
+    print(listMonth)
+
 if __name__ == '__main__':
-    quotes_temp = pd.read_csv(r'./quotes.csv', sep = ',', encoding = 'utf8')
-    quotes = np.array(quotes_temp)
-    attribute = ['date', 'open', 'low', 'high', 'close', 'volume']
-    quotesdf = pd.DataFrame(quotes, columns = attribute)
-    list1 = []
-    for i in range(0, len(quotes)):
-        x = time.strftime('%y/%m/%d', time.strptime(quotes[i][0], '%Y-%m-%d'))
-        list1.append(x)
-    quotesdf.index = list1
-    quotesdf = quotesdf.drop(['date'], axis=1)
-    df = pd.DataFrame([[1,2,3],[4,5,6],[7,8,9]],columns = ['a','b','c'])
-    temp = quotesdf['16/06/27':'16/12/27'].sort_values('close', ascending = False)[:5]
-    tempdf = quotesdf['16/06/27':'17/06/27']
-    list2 = []
-    for i in range(len(tempdf)):
-        list2.append(int(tempdf.index[i][3:5]))
-    #tempdf['month'] = list2
-    #temp = tempdf[tempdf.close > tempdf.open]['month'].value_counts()
-    #print(temp)
-    #temp = tempdf.groupby('month')['volume'].sum()
-    sort1 = tempdf.sort_values('close')
-    temp = pd.concat([sort1[:5], sort1[-5:]])
-    temp = quotesdf['16/06/27':'16/12/31'][quotesdf.close < 49]
-    print(temp)
+    MaxClose()
+    print()
--- quotesGet.py
+++ quotesGet.py
@@ -5,11 +5,14 @@ import requests
 import re
 import json
 import pandas as pd
+import time
 from datetime import date
 
-def retrieve_quotes_historical(stock_code):
+def retrieve_quotes_historical(stock_code, startdate, enddate):
     quotes = []
-    url = 'https://finance.yahoo.com/quote/%s/history?p=%s' %(stock_code, stock_code)
+    start = time.mktime(time.strptime(startdate, '%Y-%m-%d'))
+    end = time.mktime(time.strptime(enddate, '%Y-%m-%d'))
+    url = 'https://finance.yahoo.com/quote/%s/history?period1=%d&period2=%d&interval=1d&filter=history&frequency=1d' %(stock_code, start, end)
     r = requests.get(url)
     m = re.findall('"HistoricalPriceStore":{"prices":(.*),"isPending"', r.text)
     if m:
@@ -17,14 +20,15 @@ def retrieve_quotes_historical(stock_code):
         quotes = quotes[::-1]
     return [item for item in quotes if not 'type' in item]
 
-quotes = retrieve_quotes_historical('INTC')
-list1 = []
-for item in quotes:
-    x = date.fromtimestamp(item['date'])
-    y = date.strftime(x, '%Y-%m-%d')
-    list1.append(y)
-quotesdf_ori = pd.DataFrame(quotes, index = list1)
-quotesdf_m = quotesdf_ori.drop(['unadjclose'], axis=1)
-quotesdf = quotesdf_m.drop(['date'], axis=1)
-u = ['open', 'low', 'high', 'close', 'volume']
-print(quotesdf.ix[:, u].to_csv(r'./quotes_INTC.csv', sep=',', encoding='utf8', index_label='date'))
+if __name__ == '__main__':
+    quotes = retrieve_quotes_historical('INTC', '2016-1-1', '2016-12-31')
+    list1 = []
+    for item in quotes:
+        x = date.fromtimestamp(item['date'])
+        y = date.strftime(x, '%Y-%m-%d')
+        list1.append(y)
+    quotesdf_ori = pd.DataFrame(quotes, index = list1)
+    quotesdf_m = quotesdf_ori.drop(['unadjclose'], axis=1)
+    quotesdf = quotesdf_m.drop(['date'], axis=1)
+    u = ['open', 'low', 'high', 'close', 'volume']
+    print(quotesdf.ix[:, u].to_csv(r'./quotes_INTC.csv', sep=',', encoding='utf8', index_label='date'))
