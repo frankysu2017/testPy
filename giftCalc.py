7eb016dd730e7e86220bf814bf9eebe5e2e00211
frankysu2017 <frankysu@protonmail.com>
2018-02-17 08:18:48 +0800

    Merge branch 'master' of https://github.com/frankysu2017/testPy
    # Conflicts:
    #	judgeThePassword.py

--- giftCalc.py
+++ giftCalc.py
@@ -6,14 +6,15 @@ from math import floor
 from functools import reduce
 
 
-def loaddata():
-    goodsnum = 53
-    goodscode = [181350, 178796, 118814, 120676, 207246, 206534, 131816, 16102, 86328, 175181, 41582, 92303,
+class Goods(object):
+
+    def __init__(self, index):
+        __GOODSCODE__ = [181350, 178796, 118814, 120676, 207246, 206534, 131816, 16102, 86328, 175181, 41582, 92303,
                  119580, 177007, 138863, 108479, 105121, 71349, 147559, 168826, 173608, 204050, 204051, 213000,
                  216438, 188540, 184898, 204538, 202898, 117102, 148146, 210086, 109249, 115133, 203504, 139608,
                  217079, 219350, 179019, 126714, 126718, 144100, 144101, 144102, 174318, 210158, 217450, 3855, 153836,
                  180211, 999991, 999992, 999995]
-    goodsname = ['德清源安心生态鲜鸡蛋礼盒60枚装2580g', '正大鸡蛋60枚', '百年栗园鸡蛋60枚', '晓仙白洋淀柴鸡蛋60枚3000g',
+        __GOODSNAME__ = ['德清源安心生态鲜鸡蛋礼盒60枚装2580g', '正大鸡蛋60枚', '百年栗园鸡蛋60枚', '晓仙白洋淀柴鸡蛋60枚3000g',
                 '百年栗园散养柴鸡蛋48枚礼盒装2.06kg', '千禧农柴鸡蛋礼盒60枚2.75kg', '晓仙白洋淀柴鸡蛋40枚2000g',
                 '5.436鲁花花生油', '5L鲁花花生油', '鲁花压榨特香菜籽油5L', '鲁花压榨一级花生油2.5L', '5L多力葵花籽油',
                 '5L长寿花花生油', '欣奇曲有机亚麻籽油700ml', '贝蒂斯特级初榨橄榄没750ml', '多力芥花油5L',
@@ -26,10 +27,33 @@ def loaddata():
                 '万佳利加州原野茗果世界精选干果礼盒2.158kg', '万佳利加州原野福贵年年礼盒2.188kg', '万佳利加州原野锦绣年华礼盒1.535kg',
                 '果园老农添香礼盒1.56kg', '芭提娅果汁礼盒1L*4', '露露杏仁露240ml', '养元六个核桃核桃乳240ml', '养元六个核桃无糖型核桃乳240ml',
                 '10元超市券', '20元超市券', '50元超市券']
-    goodsprice = [86, 84, 70, 59.9, 59.9, 59.9, 49.8, 149.9, 139, 85.9, 74.9, 69.9, 99, 139, 129, 82.9, 74,
+        __GOODSPRICE__ = [86, 84, 70, 59.9, 59.9, 59.9, 49.8, 149.9, 139, 85.9, 74.9, 69.9, 99, 139, 129, 82.9, 74,
                   95, 29.9, 29, 38, 46, 28.5, 38.9, 39, 105, 88, 36, 45, 75, 52, 36, 60, 60, 50, 49, 42, 82,
                   56, 199, 148, 198, 168, 178, 228, 199, 78, 79, 68, 95, 10, 20, 50]
-    return goodsnum, goodscode, goodsname, goodsprice
+        #__GOODSNUM__ = len(__GOODSCODE__)
+
+        self.code = __GOODSCODE__[index]
+        self.name = __GOODSNAME__[index]
+        self.price = __GOODSPRICE__[index]
+        self.num = len(__GOODSCODE__)
+
+    def show_goods(self, file=None):
+        print("name: {:30}, code: {:^10}, price: {:^6}".format(self.name, self.code, self.price), file=file)
+
+
+def calctotalprice(goodslist):
+    return reduce(lambda x, y: x+y, map(lambda x: x.price, goodslist))
+
+
+def getgoodsindex(goods):
+    __GOODSCODE__ = [181350, 178796, 118814, 120676, 207246, 206534, 131816, 16102, 86328, 175181, 41582, 92303,
+                     119580, 177007, 138863, 108479, 105121, 71349, 147559, 168826, 173608, 204050, 204051, 213000,
+                     216438, 188540, 184898, 204538, 202898, 117102, 148146, 210086, 109249, 115133, 203504, 139608,
+                     217079, 219350, 179019, 126714, 126718, 144100, 144101, 144102, 174318, 210158, 217450, 3855,
+                     153836, 180211, 999991, 999992, 999995]
+    return __GOODSCODE__.index(goods.code)
+
+
 
 '''
 class Goods(object):
@@ -99,6 +123,8 @@ def queryBag():
     pass
 '''
 
+
+'''
 def count_one(bin_list):
     count = 0
     for i in bin_list:
@@ -141,7 +167,7 @@ def show_list(bin_list, file):
     print(lst, end="\t:  ", file=file)
 
 
-if __name__ == '__main__':
+def calc2():
     num, code, name, price = loaddata()
     count = reduce(lambda x, y: x+y, [floor(500/i) for i in price])
     print(count)
@@ -155,4 +181,35 @@ if __name__ == '__main__':
                 #print('got one!')
                 show_list(temp, f)
                 print(count_money(temp), file=f)
+    f.close()
+'''
+
+
+if __name__ == '__main__':
+    alllist = []
+    for i in range(53):
+        alllist.append([Goods(i)])
+    flag = 1
+    f = open(r'cutline.txt', 'w')
+    while flag == 1:
+        count = 0
+        templist = []
+        for glist in alllist:
+            flag = 0
+            index = getgoodsindex(glist[-1])
+            for i in range(index, 53):
+                newlist = glist.copy()
+                newlist.append(Goods(i))
+                listprice = calctotalprice(newlist)
+                if listprice <= 500:
+                    flag = 1
+                    templist.append(newlist)
+                    if listprice > 490:
+                        count += 1
+                        for goods in newlist:
+                            goods.show_goods(file=f)
+                        print('total: %0.1f---------------------------------------------' %listprice, file=f)
+        alllist = templist.copy()
+        print(len(alllist),  end='\t\t')
+        print(count)
     f.close()
