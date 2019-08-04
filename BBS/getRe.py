4ab0da867d7ce0e1c11419b0e9b9654c0aa96e6b
frankysu <frankysu@protonmail.com>
2019-08-04 22:21:52 +0800

    push

--- BBS/getRe.py
+++ BBS/getRe.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import re
+
+if __name__ == '__main__':
+    s = 'abc, aaa, "bbb, 123, cc", abc, "ddd, 666, hhh", xcf'
+    p = re.compile('"(.+?)"')
+    l = re.findall(p, s)
+    for item in l:
+        s = s.replace(item, item.replace(',', '|'))
+    print(s)
